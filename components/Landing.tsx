4726af57b364f743b9b38dde43d47805d290c0cd
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-04-18 01:16:41 -0500

    Update changes

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,15 +1,26 @@
 import Header from "@/components/Header";
 import "@/styles/landing.css"
+import Image from "next/image";
 
 export default function Landing() {
   return (
-    <article className="w-full h-[100vh] landing-background">
-      <Header />
+    <article className="w-full h-[100vh] relative">
+      <Image
+        src="./landing-bg.jpg"
+        alt="Landing Background"
+        layout="fill"
+        objectFit="cover"
+        className="z-0"
+      />
+      <div className="absolute inset-0 landing-background"></div>
+      <div className="absolute inset-0 z-20">
+        <Header />
 
-      <div className="flex flex-col items-center justify-center h-full text-white">
-        <h1 className="text-5xl font-bold mb-4">Applying filters to Lenna</h1>
-        <p className="text-xl mb-8">CS4016 - Computer Graphics - PC1</p>
+        <div className="flex flex-col items-center justify-center h-full text-white">
+          <h1 className="text-5xl font-bold mb-4">Applying filters to Lenna</h1>
+          <p className="text-xl mb-8">CS4016 - Computer Graphics - PC1</p>
+        </div>
       </div>
     </article>
-  )
+  );
 }
