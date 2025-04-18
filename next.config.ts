d096556070ae834de2b73fc37144c4ed42112ace
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-04-18 01:01:39 -0500

    Update changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/image-filters" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/image-filters/" : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
